084054d6f7641ade2fc8ce8cd6cd5fd76b64925e
ZureTz <2819058133@qq.com>
2025-05-23 11:06:19 +0800

    chore: init

---
+++ backend/xmake.lua
@@ -0,0 +1,12 @@
+-- Generate compile_commands.json for clangd every time the project is built
+add_rules("plugin.compile_commands.autoupdate", {lsp = "clangd"})
+-- Set c++ code standard: c++20
+set_languages("c++20")
+add_requires("drogon")
+
+target("simdisk")
+  set_kind("binary")
+  -- add_includedirs("include")
+  add_files("main.cc")
+  add_packages("drogon")
+
